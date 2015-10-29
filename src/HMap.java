79a3e5e71d76ad75980d88defca69effc04c17b8
TimelyToga <tblumberg39@gmail.com>
2015-10-29 19:21:57 -0400

    Finished up to test cases

--- src/HMap.java
+++ src/HMap.java
@@ -1,4 +1,61 @@
+import java.lang.Math;
 
 public class HMap {
+	
+	private int numItems;
+	private int size;
+	private Object[] values;
+	
+	/* Problem definition
+	constructor(size): return an instance of the class with pre-allocated space for the 
+		given number of objects.
+	boolean set(key, value): stores the given key/value pair in the hash map. Returns a 
+		boolean value indicating success / failure of the operation.
+	get(key): return the value associated with the given key, or null if no value is set.
+	delete(key): delete the value associated with the given key, returning the value on 
+		success or null if the key has no value.
+	float load(): return a float value representing the load factor (`(items in hash map)/(size of hash map)`) of the data structure
+		Since the size of the dat structure is fixed, this should never be greater than 1.
+	*/
+	
+	public HMap(int size){
+		this.numItems = 0;
+		this.size = size;
+		values = new Object[size];
+	}
+	
+	public boolean set(String key, Object value){
+		int arrayKey = getHash(key);
+		if(values[arrayKey] == null){
+			values[arrayKey] = value;
+			System.out.println(values[arrayKey]);
+			numItems++;
+			return true;
+		}
+		return false;
+	}
+	
+	public Object get(String key){
+		int arrayKey = getHash(key);
+		return values[arrayKey];
+	}
+	
+	public Object delete(String key){
+		int arrayKey = getHash(key);
+		Object o = values[arrayKey];
+		if(o != null){
+			numItems--;
+		}
+		
+		return o;
+	}
+	
+	public float load(){
+		return (float) numItems / size; 
+	}
+	
+	private int getHash(String key){
+		return Math.abs(key.hashCode()) % size;
+	}
 
 }
--- src/TesterClass.java
+++ src/TesterClass.java
@@ -1,7 +1,57 @@
-
+import java.util.Random;
 public class TesterClass {
+	
+	final static int NUM_TESTS = 1;
+	final static int LOAD_INTERVAL_BOUND = 90;
+	final static int MIN_LOAD = 10;
+	static int numPassed = 0;
 
-	public static void main(String[] args){
-		System.out.println("Hey, bitch");
+	public static void main(String[] args){		
+		TesterClass t = new TesterClass();
+		Random rgen = new Random();
+		
+		////// Unit Testing \\\\\\
+		// Can Store a value
+		HMap testMap = new HMap(10);
+		Item first = t.new Item("my_brain", 20);
+		
+		assertEquals("Setting Item", testMap.set("FIRST_KEY", first));
+		assertEquals("Get Item", first.equals((Item) testMap.get("FIRST_KEY")));
+
+		// Stress Test
+		int tMisses, tPlacements, tTrials, tDeletes, tSets, tGets;
+//		for(int a = 0; a < NUM_TESTS; a++){
+//			int curHMapSize = rgen.nextInt(LOAD_INTERVAL_BOUND) + MIN_LOAD;
+//			int cMisses, cPlacements, cTrials, cDeletes, cSets, cGets;
+//
+//			int curNumTrials = rgen.nextInt(curHMapSize) + curHMapSize;
+//			HMap curHMap = new HMap(curHMapSize);
+//			for(int b = 0; b < curNumTrials; b++){
+//				
+//			}
+//		}
+		
 	}
+	
+	private static void assertEquals(String name, boolean condition){
+		System.out.print(name + ": ");
+		if(condition){
+			numPassed++;
+			System.out.print("PASSED");
+		} else {
+			System.out.print("FAILED");
+		}
+		System.out.print("\n");
+	}
+	
+	class Item {
+		String name;
+		int monthsOld;
+		
+		public Item(String n, int m){
+			name = n;
+			monthsOld = m;
+		}
+	}
+	
 }
